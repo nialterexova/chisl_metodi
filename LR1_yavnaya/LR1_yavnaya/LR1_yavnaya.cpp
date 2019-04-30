d9699f041400db15be3d99991aebcb570897ed71
Oxana <34689920+nialterexova@users.noreply.github.com>
2019-04-30 13:34:08 +0300

    1

---
+++ LR1_yavnaya/LR1_yavnaya/LR1_yavnaya.cpp
@@ -0,0 +1,102 @@
+﻿
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void analit(double Tmax, double Xmax, double hx, double ht, int Nx, int Nt, double** U) {
+	ofstream f;
+	f.open("analyt.csv");
+	ofstream Err;
+	Err.open("error.csv");
+
+	double pi = 3.1416;
+	double pi2 = pi * pi;
+	double c[8];
+	double** v = new double* [Nt + 1];
+	for (int i = 0; i < Nt + 1; i++) {
+		v[i] = new double[Nx];
+	}
+	for (int k = 1; k < 8; k++) {
+		if (k % 2 == 0)  c[k] = 0;
+		else c[k] = 60 * 2 / (pi2 * k * k);
+	}
+	for (int n = 0; n <= Nt; n++) {
+		double sumErr = 0;
+		for (int j = 0; j < Nx; j++) {
+			v[n][j] = 30/2;
+			for (int k=1; k < 8; k++) v[n][j] += c[k] * exp(-pi2 * k * k * 10 * n * ht / 900) * cos(pi * k * j * hx / 30);
+			f << v[n][j] << "; ";
+			sumErr += (v[n][j] - U[n][j]) * (v[n][j] - U[n][j]);
+		}
+		f << endl;
+		Err << n * ht << "; " << sqrt(sumErr) << endl;
+	}
+	f.close();
+	Err.close();
+}
+
+int main()
+{
+	ofstream fout;
+	fout.open("lr1.csv");
+
+	double Tmax, Xmax = 30;
+	double hx = 0, ht = 0;
+	/*cout << "Enter Tmax" << endl;
+	cin >> Tmax;
+    cout << "Enter hx and ht" << endl;
+	cin >> hx >> ht;*/
+	Tmax = 150; hx = 1; ht = 0.05;
+
+	
+	int Nx = 1 + Xmax / hx;
+	int Nt = Tmax / ht;
+	fout << "Nx = " << Nx << endl;
+	fout << "Nt = " << Nt << endl;
+
+	int j, n = 0;
+	double** U = new double* [Nt+1];
+	for (int i = 0; i < Nt+1; i++) {
+		U[i] = new double[Nx];
+	}
+
+	for (j = 0; j < Nx; j++) {
+		U[0][j] = 30 - hx * j ;
+	}
+
+	while (n<Nt)
+	{
+		for (j = 1; j < Nx-1; j++) {
+			U[n + 1][j] = U[n][j] + (10 * ht * (U[n][j + 1] - 2 * U[n][j] + U[n][j - 1])) / (hx * hx);
+		}
+		U[n + 1][0] = U[n + 1][1];
+		U[n + 1][Nx-1] = U[n + 1][Nx-2];
+		n++;
+	}
+
+	for (n = 0; n <= Nt; n++) {
+		if (n == 0) {
+			fout << "n/j; ";
+			for (j = 0; j < Nx; j++) fout << hx * j << "; ";
+			fout << endl;
+		}
+		fout << n << "(" << n*ht <<"); ";
+		for (j = 0; j < Nx; j++) {
+			fout << U[n][j] << "; ";
+		}
+		fout << endl;
+	}
+
+	//--------analit-------------
+
+	analit(Tmax, Xmax, hx, ht, Nx, Nt, U);
+
+	//--------analit-------------
+
+	for (int i = 0; i <= Nt; i++) {
+		delete[]U[i];
+	}
+
+	fout.close();
+}
---
+++ LR2/LR2/LR2.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void analitic(double Tmax, double Xmax, double hx, double dt, int Nx, int Nt, double** u) {
+	ofstream f;
+	f.open("analytic.csv");	//файл для записи резуьтатов, посчитанных аналитически
+	ofstream ErrorFile;
+	ErrorFile.open("error.csv"); //файл для записи результатов погрешности решения
+
+	double pi = 3.1416;
+	double pi2 = pi * pi;
+	double c[8];
+
+	double** v = new double* [Nt + 1];
+	for (int i = 0; i < Nt + 1; i++) {
+		v[i] = new double[Nx];		//создание массива
+	}
+
+	for (int k = 1; k < 8; k++) {
+		if (k % 2 == 0)  c[k] = 0;
+		else c[k] = -100 / (pi2 * k * k);		//определение коэффициента Ск
+	}
+
+	for (int n = 0; n <= Nt; n++) {
+		double sumErr = 0;
+		for (int j = 0; j < Nx; j++) {
+			v[n][j] = 25 / 2;
+			for (int k = 1; k < 8; k++) v[n][j] += c[k] * exp(-pi2 * k * k * 1.14 * n * dt / (25 * 25)) * cos(pi * k * j * hx / 25);	//определение аналит решения
+			f << v[n][j] << "; ";		//вывод в файл аналит решения точки 
+			sumErr += (v[n][j] - u[n][j]) * (v[n][j] - u[n][j]);		//суммирование ошибки для данной итерации по времени
+		}
+		f << endl;
+		ErrorFile << n * dt << "; " << sqrt(sumErr) << endl;		//вывод в файл ошибки для итерации по времени
+	}
+	f.close();
+	ErrorFile.close();
+}
+
+int main()
+{
+	ofstream file;
+	file.open("result.csv");
+
+	double Xmax, Tmax, hx, dt;
+	Xmax = 24; Tmax = 180; hx = 0.5; dt = 1;			//задание начальных условий
+
+	int Nx = 1 + Xmax / hx;			//вычисление количества итераций
+	int Nt = Tmax / dt;
+
+	int j, n = 0;
+	double** u = new double* [Nt + 1];
+	for (int i = 0; i < Nt + 1; i++) {
+		u[i] = new double[Nx];		//выделение памяти под массив 
+	}
+
+	double a, b, c, e;
+	a = -1.14 * dt / hx / hx;
+	c = a;
+	b = 1 + 1.14*2 * dt / hx / hx;
+	double* Ka = new double[Nx];
+	double* Kb = new double[Nx];
+
+	for (j = 0; j < Nx; j++) {
+		u[0][j] = hx * j;		//начальные условия
+	}
+
+	while (n < Nt)
+	{
+		Ka[0] = 1;
+		Kb[0] = 0;
+		for (j = 1; j < Nx - 1; j++) {
+			e = u[n][j];
+			Ka[j] = -a / (b + c * Ka[j - 1]);
+			Kb[j] = (e - c * Kb[j - 1]) / (b + c * Ka[j - 1]);
+		}
+		u[n + 1][Nx - 1] = Kb[Nx - 2] / (1 - Ka[Nx - 2]);
+		for (j = Nx - 2; j >= 0; j--) {
+			u[n + 1][j] = Ka[j] * u[n + 1][j + 1] + Kb[j];
+		}
+		n++;
+	}
+
+	for (n = 0; n <= Nt; n++) {
+		for (j = 0; j < Nx; j++) {
+			file << u[n][j] << "; ";	//вывод результата 
+		}
+		file << endl;
+	}
+
+	analitic(Tmax, Xmax, hx, dt, Nx, Nt, u);	//Запуск аналитического решения
+
+	for (int i = 0; i <= Nt; i++) {
+		delete[]u[i];
+	}
+
+	file.close();
+}
